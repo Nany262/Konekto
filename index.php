7440bd248a9f69fa32dc4ea1e408417dbdff606f
Daniela Zuluaga <daniela-zuluaga@utp.edu.co>
2016-05-12 17:40:50 -0500

    Login

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype <!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+	<title>Konekto</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="../../favicon.ico">
+	<link href="statics/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+<h1>Konekto</h1><br>
+<form action="inicio.php" method="post">
+	<label><input class= "form-control" type="email" name="correo" placeholder="Correo Electronico"></center><br>
+	<label><input class= "form-control" type="password" name="password" placeholder="Contraseña"></center><br>
+	<button type="submit" name="datos" value="Iniciar"></button>
+</div>
+</body>
+</html>>
+
